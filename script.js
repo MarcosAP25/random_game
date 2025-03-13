f046eff36124a6a27c0067473d26c30b3f2b3338
Disant444 <sdionis665@gmail.com>
2025-03-13 00:20:51 -0400

    Trabajo completo

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // Variables globales
+let inputNum;
+let contIntentos = 0;
+let numAdivinar;
 
 
 
@@ -26,18 +29,19 @@ userGuess.addEventListener('keypress', function(event) {
  */
 function startNewGame() {
     // Generar número aleatorio entre 1 y 100
-    
-    
+    numAdivinar =  Math.floor(Math.random()*100)+1
     
     
     // Actualizar interfaz
-    attemptsElement.textContent = attempts;
+    contIntentos = 0;
+    attemptsElement.textContent = contIntentos;
     messageElement.textContent = '';
     messageElement.className = 'message';
     userGuess.value = '';
     userGuess.focus();
+
     
-    console.log('Nuevo juego iniciado. Número secreto:', randomNumber);
+    console.log('Nuevo juego iniciado. Número secreto:', numAdivinar);
 }
 
 /**
@@ -48,29 +52,47 @@ function checkGuess() {
   
     // Obtener el valor ingresado por el usuario
     //El valor se obtiene de (userGuess.value)
-    
+    inputNum = userGuess.value
     
     // Validar entrada
     
+    if (inputNum < 1 || inputNum > 100){
+        messageElement.textContent = `EL NUMERO INGRESADO, ESTA FUERA DE RANGO`;
+    };
     
     // Incrementar intentos
     
-    attemptsElement.textContent = attempts; //Incremento en el dom
+    attemptsElement.textContent = contIntentos++; //Incremento en el dom
+
+
     
     // Comprobar resultado
     
         // Acierto
-        messageElement.textContent = `¡Felicidades! Has adivinado el número ${randomNumber} en ${attempts} intentos.`;
-        messageElement.className = 'message correct';
+
+        if(inputNum == numAdivinar){
+            messageElement.textContent = `¡Felicidades! Has adivinado el número ${numAdivinar} en ${contIntentos} intentos.`;
+            messageElement.className = 'message correct';   
+        };
     
     
         // Número demasiado alto
-        messageElement.textContent = `El número ${guess} es MAYOR que el número secreto.`;
-        messageElement.className = 'message higher';
+        if(inputNum > numAdivinar ){
+            attemptsElement.textContent = contIntentos;
+            messageElement.textContent = `El número ${inputNum} es MAYOR que el número secreto.`;
+            messageElement.className = 'message higher';
+            userGuess.value = '';
+
+        };
     
         // Número demasiado bajo
-        messageElement.textContent = `El número ${guess} es MENOR que el número secreto.`;
-        messageElement.className = 'message lower';
+        if(inputNum < numAdivinar ){
+            attemptsElement.textContent = contIntentos;
+            messageElement.textContent = `El número ${inputNum} es MENOR que el número secreto.`;
+            messageElement.className = 'message lower';
+            userGuess.value = '';
+        };
+
     
     
     // Limpiar input y enfocar
